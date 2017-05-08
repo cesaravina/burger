ecef73b9fb998c79134ef4d4a14df7deeaac2634
cesar avina <cesaravina@gmail.com>
2017-05-07 18:04:32 -0700

    conection

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
 // Dependencies
-var connection = require("../connection.js");
+var connection = require("./connection.js");
 
 // SQL syntax helper
 function printQuestionMarks(num){
@@ -23,20 +23,52 @@ function objToSql(obj){
 	return arr.toString();
 }
 
+// ORM / SQL statments object
+var orm = {
+	all: function(tableInput, cb){
+		var queryString = "SELECT * FROM" + tableInput + ";";
+		connection.query(queryString, function(err, result){
+			if(err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+	create: function(table, cols, vals, cb){
+		var queryString = "INSERT INTO " + table;
 
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
 
+		console.log(queryString);
 
+		connection.query(queryString, vals, function(err, result){
+			if (err){
+				throw err;
+			}
+			cb(result);
+		});
+	},
+	update: function(table, objColVals, condition, cb){
+		var queryString = "UPDATE " + table;
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += "WHERE";
+		queryString += condition
 
+		console.log(queryString);
+		connection.query(queryString, function(err, result){
+			if(err){
+				throw err;
+			}
+			cb(result);
+		});
+	}
+};
 
-
-
-
-
-
-
-
-
-
-
-
+// Export orm object to burger.js
 module.exports = orm;
